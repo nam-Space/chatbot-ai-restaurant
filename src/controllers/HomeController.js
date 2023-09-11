0cd03f8298b8681b6764146c3cbe7e4ffb4dcf9e
NITRO-AN515-56\Nam <namyahoo2003@gmail.com>
2023-09-12 00:42:39 +0700

    add more messenger plugin

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -122,6 +122,10 @@ function handlePostback(sender_psid, received_postback) {
         response = { text: "Thanks!" };
     } else if (payload === "no") {
         response = { text: "Oops, try sending another image." };
+    } else if (payload === "GET_STARTED") {
+        response = {
+            text: "OK! Chào mừng bạn ABC đến với nhà hàng của Nam Nguyễn",
+        };
     }
     // Send the message to acknowledge the postback
     callSendAPI(sender_psid, response);
