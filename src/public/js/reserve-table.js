123326de4373d8c54b475071acbe0b1d6fe66deb
NITRO-AN515-56\Nam <namyahoo2003@gmail.com>
2023-09-16 20:49:30 +0700

    change app id

--- src/public/js/reserve-table.js
+++ src/public/js/reserve-table.js
@@ -1,15 +1,20 @@
 (function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) { return; }
-    js = d.createElement(s); js.id = id;
+    var js,
+        fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {
+        return;
+    }
+    js = d.createElement(s);
+    js.id = id;
     js.src = "//connect.facebook.net/en_US/messenger.Extensions.js";
     fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'Messenger'));
+})(document, "script", "Messenger");
 
 window.extAsyncInit = function () {
-    // the Messenger Extensions JS SDK is done loading 
+    // the Messenger Extensions JS SDK is done loading
 
-    MessengerExtensions.getContext('753933095237424',
+    MessengerExtensions.getContext(
+        process.env.APP_ID,
         function success(thread_context) {
             // success
             //set psid to input
@@ -18,14 +23,15 @@ window.extAsyncInit = function () {
         },
         function error(err) {
             // error
-            console.log('Lỗi đặt bàn Eric bot', err);
+            console.log("Lỗi đặt bàn Eric bot", err);
         }
     );
 };
 
 //validate inputs
 function validateInputFields() {
-    const EMAIL_REG = /[a-zA-Z][a-zA-Z0-9_\.]{1,32}@[a-z0-9]{2,}(\.[a-z0-9]{2,4}){1,2}/g;
+    const EMAIL_REG =
+        /[a-zA-Z][a-zA-Z0-9_\.]{1,32}@[a-z0-9]{2,}(\.[a-z0-9]{2,4}){1,2}/g;
 
     let email = $("#email");
     let phoneNumber = $("#phoneNumber");
@@ -47,7 +53,6 @@ function validateInputFields() {
     return false;
 }
 
-
 function handleClickButtonReserveTable() {
     $("#btnReserveTable").on("click", function (e) {
         let check = validateInputFields(); //return true or false
@@ -56,19 +61,22 @@ function handleClickButtonReserveTable() {
             psid: $("#psid").val(),
             customerName: $("#customerName").val(),
             email: $("#email").val(),
-            phoneNumber: $("#phoneNumber").val()
+            phoneNumber: $("#phoneNumber").val(),
         };
 
         if (!check) {
             //close webview
-            MessengerExtensions.requestCloseBrowser(function success() {
-                // webview closed
-            }, function error(err) {
-                // an error occurred
-                console.log(err);
-            });
+            MessengerExtensions.requestCloseBrowser(
+                function success() {
+                    // webview closed
+                },
+                function error(err) {
+                    // an error occurred
+                    console.log(err);
+                }
+            );
 
-            //send data to node.js server 
+            //send data to node.js server
             $.ajax({
                 url: `${window.location.origin}/reserve-table-ajax`,
                 method: "POST",
@@ -78,8 +86,8 @@ function handleClickButtonReserveTable() {
                 },
                 error: function (error) {
                     console.log(error);
-                }
-            })
+                },
+            });
         }
     });
 }
